39817fc2d244f0606977118a05fbdd36c5e4a172
jdeliaDev <jdelia@bld.com.ar>
2022-11-16 12:21:07 -0300

    actualizar

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let btnEnviar = document.querySelector(".btn-form-enviar");
+let btnProductos = document.querySelector("#productos");
+let subMenu = document.querySelector("#subMenu");*/
+let btnProductosFooter = document.querySelector("#productosFooter");
+let subMenuFooter = document.querySelector("#subMenuFooter");
+
+/*btnProductos.addEventListener("click",()=>{
+    subMenu.classList.toggle("mostrar")    
+})*/
+
+btnProductosFooter.addEventListener("click",()=>{
+    subMenuFooter.classList.toggle("mostrar")    
+})
+
+/*btnEnviar.addEventListener("click",()=>{
+    window.location.href="./Productos.html"
+});*/
+
